b655243c3343d6ab486a7a83e2f937843f575651
Вещин Олег Олегович <Veschin.283asp@stud.asu.ru>
2020-02-26 17:31:58 +0700

    init

---
+++ generator.py
@@ -0,0 +1,34 @@
+import random
+
+class  Driver_generator():
+    
+    self.driver = {
+        "name": "",
+        "lname": "",
+        "experience": "",
+        "fine": "",
+        "commendation": ""
+    }
+
+    self.auto = {
+        "brand": "",
+        "color": "",
+        "number": ""
+    }
+
+    self.night_call = {
+        "date": "",
+        "length": "",
+        "call_area": "",
+        "destination_area": "",
+        "price": ""
+    }
+
+    def get_person():
+        pass
+
+    def get_night_call():
+        pass
+
+    def get_auto():
+        pass
